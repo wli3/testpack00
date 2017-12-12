2c66c097d5a9a2faeaa8808c775ae4dd0a6a7d48
William Li <wul@microsoft.com>
2017-12-11 17:00:52 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace consoledemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = "Hello World from Global Tool";
+            Console.WriteLine(greeting);
+        }
+    }
+}
